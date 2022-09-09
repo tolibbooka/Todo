49119bc1b708a7dbf15b37ef5216447faadaa0d2
tolibbooka <tgulomjanov1@gmail.com>
2022-09-09 16:27:24 +0500

    todolist

--- src/components/TodolistForm.js
+++ src/components/TodolistForm.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { useState } from 'react';
 
-export  function TodolistForm(props) {
+export function TodolistForm(props) {
     const [input,setInput]= useState('');
+    const [harakat, setHarakat] = useState('')
+    const [stil, setStil] = useState({});
 
+
+    const tutish = (e) => {
+        setHarakat(e.target.value)
+    }
+    const submit = (e) => {
+        e.preventDefault();
+
+        let stil = {};
+        if (harakat === '') {
+            stil = {
+                border: "2px solid red",
+            };
+    }
+        else {
+        stil = {
+            border: "3px solid green",
+        };
+    }
+    setStil(stil);
+}
     const handleSubmit = (e) =>{
         e.preventDefault()
         props.addTodo(input)
@@ -11,14 +33,14 @@ export  function TodolistForm(props) {
     }
 
     return (
-        <form onSubmit={handleSubmit} className='todo-form'>
+        <form onSubmit={submit} onSubmit={handleSubmit} className='todo-form'>
             <input 
             value ={input}
             onChange={(e) => setInput (e.target.value)}
             className='todo-input'
             placeholder='Add a Todo'
             />
-            <button type='submit' className='todo-button'>Add Todo</button>
+            <button type='submit' className='todo-button'style={stil} onChange={tutish}>Add Todo</button>
         </form>
      
     );
